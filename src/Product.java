8bb0b6c9445cff5e4024fca7539d6d4dd4effc4e
Fillnet <filipp@tenikaytis.ru>
2022-11-30 22:48:39 +0300

    HW_HashMap_HW2

--- src/Product.java
+++ src/Product.java
@@ -3,17 +3,15 @@ import java.util.Objects;
 public class Product {
     private final String name;
     private final float price;
-    private final int quantity;
-    private boolean checked;
 
-    public Product(String name, float price, int quantity) {
-        if (name == null || name.isBlank() || price < 0 || quantity < 0) {
+
+    public Product(String name, float price) {
+        if (name == null || name.isBlank() || price < 0 ) {
             throw new IllegalArgumentException("Заполните карточку продукта");
         }
         this.name = name;
         this.price = price;
-        this.quantity = quantity;
-        this.checked = false;
+
     }
 
     public String getName() {
@@ -24,14 +22,7 @@ public class Product {
         return price;
     }
 
-    public int getQuantity() {
-        return quantity;
-    }
 
-    public void setChecked() {
-        this.checked = true;
-    }
-    public boolean isCheked(){return checked;}
 
     @Override
     public boolean equals(Object o) {
@@ -53,16 +44,8 @@ public class Product {
 
     @Override
     public String toString() {
-        String checkedString = this.isCheked() ? "Да" : "Нет";
-        return String.format("%s, Цена: %s, Количество: %s, Куплен: %s", this.name, this.price, this.quantity, checkedString);
+        return String.format("%s, Цена: %s", this.name, this.price);
     }
 
-    public static class ProductAlreadyExistException extends Exception {
-        public ProductAlreadyExistException(String message) {
 
-            System.out.printf("Продукт %s уже добавлен в список \n", message);
-
-
-        }
-    }
 }
